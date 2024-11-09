538807805c8326e004b80b3da0b19c6d41e6dafd
Lowie Verschelden <lowie@lowieverschelden.be>
2024-11-09 22:31:49 +0100

    final de ejercicio

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
  // Aquí tu código
+
+ const knopje = document.getElementById('agregar');
+ const inputje = document.getElementById('inputelemento');
+ const lijstje = document.getElementById('lista');
+
+
+ knopje.addEventListener('click',function() {
+    if(!document.getElementById('inputelementolijstje')) {
+        inputje.insertAdjacentHTML('afterbegin','<input id="inputelementolijstje">');
+    }
+    else {
+        const inputveld = document.getElementById('inputelementolijstje');
+        if(inputveld && inputveld.value!=='') {
+            lijstje.insertAdjacentHTML('afterbegin','<li>'+inputveld.value+'</li>');
+            inputveld.value='';
+        }
+    }
+})
